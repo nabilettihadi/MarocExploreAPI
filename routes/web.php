414cf1334b38e334d3c029b6739e77f4ee0530ff
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-03-29 21:31:20 +0000

    final version

--- routes/web.php
+++ routes/web.php
@@ -20,27 +20,3 @@ use App\Http\Controllers\EndroitAVisiterController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-// Routes pour l'authentification des utilisateurs
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:api');
-
-// Routes pour les itinéraires
-Route::post('/itineraires', [ItineraireController::class, 'create'])->middleware('auth:api');
-Route::post('/itineraires/{itineraireId}/addDestination', [ItineraireController::class, 'addDestination'])->middleware('auth:api');
-Route::post('/itineraires/{itineraireId}/addToVisitList', [ItineraireController::class, 'addToVisitList'])->middleware('auth:api');
-Route::get('/itineraires', [ItineraireController::class, 'index']);
-Route::get('/itineraires/search', [ItineraireController::class, 'search']);
-
-// Routes pour les destinations
-Route::post('/destinations/{itineraireId}', [DestinationController::class, 'addDestination'])->middleware('auth:api');
-
-// Routes pour les endroits à visiter
-Route::post('/endroitsAVisiter/{destinationId}', [EndroitAVisiterController::class, 'addEndroitAVisiter'])->middleware('auth:api');
-Route::get('/endroitsAVisiter/{destinationId}', [EndroitAVisiterController::class, 'getEndroitsAVisiter']);
-Route::put('/endroitsAVisiter/{endroitAVisiterId}', [EndroitAVisiterController::class, 'updateEndroitAVisiter'])->middleware('auth:api');
-Route::delete('/endroitsAVisiter/{endroitAVisiterId}', [EndroitAVisiterController::class, 'deleteEndroitAVisiter'])->middleware('auth:api');
-
